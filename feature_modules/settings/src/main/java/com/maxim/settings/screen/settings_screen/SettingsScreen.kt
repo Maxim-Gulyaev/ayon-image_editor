9e3c005e8adab5708ea78e44c3e82f78a2010bcd
Maxim <maxim_@bk.ru>
2025-12-13 18:04:30 +0300

    refactor settings screen

--- feature_modules/settings/src/main/java/com/maxim/settings/screen/settings_screen/SettingsScreen.kt
+++ feature_modules/settings/src/main/java/com/maxim/settings/screen/settings_screen/SettingsScreen.kt
@@ -1,24 +1,31 @@
 package com.maxim.settings.screen.settings_screen
 
 import androidx.annotation.StringRes
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.maxim.settings.R
 import com.maxim.settings.screen.component.SettingsTopAppBar
-import com.maxim.ui.component.ContainerCard
+import com.maxim.ui.component.AyonHorizontalSpacer
 import com.maxim.ui.theme.AyonTheme
 import com.maxim.ui.theme.AyonTypography
 import com.maxim.ui.util.AdaptivePreviewDark
 import com.maxim.ui.util.AdaptivePreviewLight
+import com.maxim.ui.util.NoRippleInteractionSource
 
 @Composable
 fun SettingsScreen(
@@ -73,9 +80,21 @@ private fun SettingsItem(
     onClick: () -> Unit,
     modifier: Modifier = Modifier,
 ) {
-    ContainerCard(
-        onClick = onClick
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+            .clickable(
+                indication = null,
+                interactionSource = NoRippleInteractionSource,
+                onClick = onClick,
+            ),
+        verticalAlignment = Alignment.CenterVertically,
     ) {
+        Icon(
+            painter = painterResource(R.drawable.ic_language),
+            contentDescription = "Language icon",
+        )
+        AyonHorizontalSpacer(12.dp)
         Text(
             modifier = modifier
                 .padding(horizontal = 12.dp, vertical = 12.dp),
