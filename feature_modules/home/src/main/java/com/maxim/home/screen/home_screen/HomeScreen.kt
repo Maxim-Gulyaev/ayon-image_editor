c74bdd16488bea8eacd73e38d5f59c1e879eca21
Maxim <maxim_@bk.ru>
2025-12-13 17:46:14 +0300

    refactor home screen

--- feature_modules/home/src/main/java/com/maxim/home/screen/home_screen/HomeScreen.kt
+++ feature_modules/home/src/main/java/com/maxim/home/screen/home_screen/HomeScreen.kt
@@ -1,10 +1,15 @@
 package com.maxim.home.screen.home_screen
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -18,12 +23,18 @@ import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.maxim.home.R
 import com.maxim.home.util.homeScreenUiStateMock
 import com.maxim.model.Jog
-import com.maxim.ui.component.AyonHorizontalSpacer
+import com.maxim.ui.component.AyonVerticalSpacer
 import com.maxim.ui.component.LoadingScreen
 import com.maxim.ui.error.AyonErrorScreen
 import com.maxim.ui.theme.AyonTheme
+import com.maxim.ui.theme.AyonTypography
 import com.maxim.ui.util.AdaptivePreviewDark
 import com.maxim.ui.util.AdaptivePreviewLight
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+import kotlin.time.Duration
+
+private val DAY_MONTH_YEAR_FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy")
 
 @Composable
 fun HomeScreen(
@@ -43,8 +54,8 @@ fun HomeScreen(
 
 @Composable
 private fun HomeScreenContent(
-    modifier: Modifier = Modifier,
     state: HomeScreenUiState,
+    modifier: Modifier = Modifier,
 ) {
     when (state) {
 
@@ -70,15 +81,37 @@ private fun HomeScreenContent(
 @Composable
 private fun JogHistoryBlock(
     jogs: List<Jog>,
+    modifier: Modifier = Modifier,
 ) {
-    LazyColumn {
-        item {
-            Text(stringResource(R.string.jog_history))
-        }
-        items(
-            items = jogs,
-        ) { item ->
-            JogHistoryItem(item.date.toString(), item.duration.toString())
+    Card(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(horizontal = 16.dp),
+    ) {
+        LazyColumn(
+            contentPadding = PaddingValues(horizontal = 24.dp, vertical = 12.dp),
+        ) {
+            item {
+                Box(
+                    modifier = Modifier.fillMaxWidth(),
+                    contentAlignment = Alignment.Center,
+                ) {
+                    Text(
+                        text = stringResource(R.string.jog_history),
+                        style = AyonTypography.titleMedium
+                    )
+                }
+            }
+
+            items(
+                items = jogs,
+            ) { item ->
+                AyonVerticalSpacer(8.dp)
+                JogHistoryItem(
+                    date = item.date.toFormattedDate(),
+                    duration = item.duration.toFormattedDuration(),
+                )
+            }
         }
     }
 }
@@ -87,14 +120,57 @@ private fun JogHistoryBlock(
 private fun JogHistoryItem(
     date: String,
     duration: String,
+    modifier: Modifier = Modifier,
 ) {
     Row {
         Text(date)
-        AyonHorizontalSpacer(16.dp)
+
+        Spacer(modifier = modifier.weight(1f))
 
         Text(duration)
     }
 }
+
+@Composable
+private fun Duration.toFormattedDuration(
+    showSeconds: Boolean = true
+): String {
+    val elapsedSeconds = inWholeSeconds
+
+    val hours = elapsedSeconds / 3600
+    val minutes = (elapsedSeconds % 3600) / 60
+    val seconds = elapsedSeconds % 60
+
+    val h = stringResource(R.string.duration_h_short)
+    val m = stringResource(R.string.duration_min_short)
+    val s = stringResource(R.string.duration_sec_short)
+
+    return buildString {
+        if (hours > 0) {
+            append(hours)
+            append(' ')
+            append(h)
+        }
+
+        if (minutes > 0) {
+            if (isNotEmpty()) append(' ')
+            append(minutes)
+            append(' ')
+            append(m)
+        }
+
+        if (showSeconds && (seconds > 0 || isEmpty())) {
+            if (isNotEmpty()) append(' ')
+            append(seconds)
+            append(' ')
+            append(s)
+        }
+    }
+}
+
+
+private fun LocalDate.toFormattedDate(): String =
+    format(DAY_MONTH_YEAR_FORMATTER)
 
 @Composable
 @AdaptivePreviewLight
--- feature_modules/home/src/main/java/com/maxim/home/util/HomeScreenPreviewMock.kt
+++ feature_modules/home/src/main/java/com/maxim/home/util/HomeScreenPreviewMock.kt
@@ -4,12 +4,12 @@ import com.maxim.home.screen.home_screen.HomeScreenUiState
 import com.maxim.model.Jog
 import kotlinx.collections.immutable.persistentListOf
 import java.time.LocalDate
-import kotlin.time.Duration.Companion.minutes
+import kotlin.time.Duration.Companion.seconds
 
 fun jogMock() =
     Jog(
         date = LocalDate.of(2025, 7, 10),
-        duration = 20.minutes,
+        duration = 358944.seconds,
     )
 
 fun homeScreenUiStateMock() =
